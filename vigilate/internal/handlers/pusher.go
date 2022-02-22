2b8242b5bb813625862953ee13e456b02b468ac9
kejiantong <tong.ke@northeastern.edu>
2022-02-21 22:00:41 -0800

    add an event to the client

--- vigilate/internal/handlers/pusher.go
+++ vigilate/internal/handlers/pusher.go
@@ -14,7 +14,7 @@ func (repo *DBRepo) PusherAuth(w http.ResponseWriter, r *http.Request) {
 
 	u, _ := repo.DB.GetUserById(userID)
 
-	params, _ := ioutil.ReadAll(r.Body)
+	params, _ := ioutil.ReadAll(r.Body)          
 
 	presenceData := pusher.MemberData{
 		UserID: strconv.Itoa(userID),
